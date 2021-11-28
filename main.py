b546e21815f5d42793ab34737ec74bfc940f5120
sbrence <simon.brence@gmail.com>
2021-11-28 13:48:57 +0100

    update

--- main.py
+++ main.py
@@ -1,45 +1,81 @@
 import subprocess
 import os
 from pathlib import Path
+import shutil
+import sys
 
-path = r"C:\Users\sbrence\PycharmProjects\gurs_snemanje_datotek_nepremicnine\dl_gurs_nepremicnine_backup_ver.1.0.py"
-w_path = Path(path)
 
-# process = subprocess.Popen(['mkdir', '123'],
+def create_exe():
+    orig_file = sys.argv[1]
+    # orig_file = r"C:\Users\sbrence\PycharmProjects\gurs_snemanje_datotek_nepremicnine\dl_gurs_nepremicnine_backup_ver.1.0.py"
+    orig_file_path = Path(orig_file)
+    file_name = orig_file.rsplit("\\", 1)[1]
+
+    # Ustvari folder z istim imenom kot je file, vendar brez .py koncnice
+    working_file_to_dir = orig_file.rsplit(".", 1)[0]
+    working_file_to_dir_path = Path(working_file_to_dir)
+
+    dist_path = Path(working_file_to_dir_path, "dist")
+    build_path = Path(working_file_to_dir_path, "build")
+    spec_path = list(working_file_to_dir_path.glob('*.spec'))
+    if spec_path:
+        spec_path = spec_path[0]
+
+
+    if dist_path.exists():
+        delete = input("Izbrisi obstojeco exe distribuicjo (d/n) ?")
+        if delete == "d":
+            shutil.rmtree(dist_path, ignore_errors=True)
+            shutil.rmtree(build_path, ignore_errors=True)
+            spec_path.unlink(missing_ok=True)
+
+            # Spremeni direktorji
+            os.chdir(working_file_to_dir)
+
+            print("Poganjam cmd ukaze...")
+            new_file_path = Path(working_file_to_dir + '\\' + file_name)
+            commands = [f'{working_file_to_dir}\\env\\Scripts\\activate.bat',
+                        f'{working_file_to_dir}\\env\\Scripts\\pyinstaller.exe --onefile {str(new_file_path)}',
+                        f'{working_file_to_dir}\\env\\Scripts\\deactivate.bat'
+                        ]
+            str_commands = ' && '.join(commands)
+            os.system(str_commands)
+            print("Zakljucil s poganjanjem cmd ukazov.")
+
+        else:
+            print("Nicesar ne bom zbrisal")
+            return None
+
+    else:
+
+        working_file_to_dir_path.mkdir(parents=True, exist_ok=True)
+        print("Ustvaril novo mapo")
+
+        # Spremeni direktorji
+        os.chdir(working_file_to_dir)
+
+        # Skopiraj file v novo mapo
+        new_file_path = Path(working_file_to_dir + '\\' + file_name)
+        shutil.copy(orig_file_path, new_file_path)  # For Python 3.8+.
+        print("Skoprila file v novo mapo")
+
+        print("Poganjam cmd ukaze...")
+        commands = ['python -m venv env', f'{working_file_to_dir}\\env\\Scripts\\activate.bat',
+                    'pip install pipreqs', f'pipreqs "{working_file_to_dir}"', 'pip install -r requirements.txt',
+                    'pip install pyinstaller', 'pip install lxml',
+                    f'{working_file_to_dir}\\env\\Scripts\\pyinstaller.exe --onefile {str(new_file_path)}',
+                    'pip freeze > requirements.txt', f'{working_file_to_dir}\\env\\Scripts\\deactivate.bat'
+                    ]
+        str_commands = ' && '.join(commands)
+        os.system(str_commands)
+        print("Zakljucil s poganjanjem cmd ukazov.")
+
+# process = subprocess.Popen(['python', '-m', 'venv', 'env'],
 #                      stdout=subprocess.PIPE,
 #                      stderr=subprocess.PIPE, shell=True)
 # stdout, stderr = process.communicate()
 # stdout, stderr
-#
-#
-# wd = os.getcwd()
-# os.chdir("/")
-# subprocess.Popen("dir")
-# os.chdir(wd)
-
-working_dir = path.rsplit("\\", 1)[0]
-working_file = path.rsplit("\\", 1)[1]
-
-# Ustvari folder z istimo imenom kot je file, vendar brez .py koncnice
-working_file_to_dir = path.rsplit(".", 1)[0]
-Path(working_file_to_dir).mkdir(parents=True, exist_ok=True)
-os.chdir(path)
 
 
-
-# # This is a sample Python script.
-#
-# # Press Shift+F10 to execute it or replace it with your code.
-# # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-#
-#
-# def print_hi(name):
-#     # Use a breakpoint in the code line below to debug your script.
-#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-#
-#
-# # Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
-#
-# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == '__main__':
+    create_exe()
